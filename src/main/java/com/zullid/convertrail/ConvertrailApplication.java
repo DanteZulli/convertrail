7d7e3ae62c2ac5edd86de709ee253d0ae1087ad7
Dante Zulli <dantezulli2004@gmail.com>
2023-06-29 12:20:14 -0300

    Initial commit

---
+++ src/main/java/com/zullid/convertrail/ConvertrailApplication.java
@@ -0,0 +1,13 @@
+package com.zullid.convertrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zullid/convertrail/ConvertrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zullid.convertrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
