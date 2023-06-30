deb31a854d4d0e55d2a42d90890a26d376a44c1d
Dante Zulli <dantezulli2004@gmail.com>
2023-06-30 16:53:04 -0300

    dementia - Easier way

--- src/main/java/com/zullid/convertrail/ConvertrailApplication.java
+++ src/main/java/com/zullid/convertrail/ConvertrailApplication.java
@@ -1,12 +1,20 @@
 package com.zullid.convertrail;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.opencsv.exceptions.CsvValidationException;
+import com.zullid.convertrail.csv.CsvDynamic;
+import com.zullid.convertrail.csv.CsvReader;
 @SpringBootApplication
 public class ConvertrailApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CsvValidationException, FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException {
 		SpringApplication.run(ConvertrailApplication.class, args);
-		
 	}
 }
--- src/main/java/com/zullid/convertrail/csv/CsvDynamic.java
+++ src/main/java/com/zullid/convertrail/csv/CsvDynamic.java
@@ -1,113 +1,47 @@
 package com.zullid.convertrail.csv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.constant.DirectMethodHandleDesc.Kind;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.SimpleJavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.StandardLocation;
-import javax.tools.ToolProvider;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.opencsv.CSVReader;
-import com.opencsv.bean.CsvBindByPosition;
-import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvValidationException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CsvDynamic {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException,
-            IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
-            SecurityException, CsvValidationException {
+    public static void main(String[] args) throws FileNotFoundException {
+
         File csvFile = CsvReader.getRandomFile();
-        generateClass(CsvReader.getHeaders(csvFile));
-        generateBean(csvFile);
-    }
+        ArrayList<Map<String, Object>> dataMap = new ArrayList<Map<String, Object>>();
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            String[] headers = reader.readNext();
+            String[] rowData;
+            while ((rowData = reader.readNext()) != null) {
+                Map<String, Object> dataEntry = new HashMap<>();
+                for (int i = 0; i <= headers.length - 1; i++) {
+                    String header = headers[i];
+                    Object value = rowData[i];
+                    dataEntry.put(header, value);
+                }
+                dataMap.add(dataEntry);
+            }
+            String jsonFilePath = "c://Users//zullid//Documents//json//data.json";
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                objectMapper.writeValue(new File(jsonFilePath), dataMap);
+                System.out.println("The array of maps has been written to " + jsonFilePath + " successfully!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-    public static void generateClass(String[] headers) throws FileNotFoundException, IOException,
-            ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
-            InvocationTargetException, NoSuchMethodException, SecurityException, CsvValidationException {
-        String className = "CsvBeans";
-
-        // Generar la definición de la clase
-        StringBuilder sb = new StringBuilder();
-        sb.append("package com.zullid.convertrail.csv;\n\n");
-        sb.append("import com.opencsv.bean.CsvBindByPosition;\n\n");
-        sb.append("public class ").append(className).append(" {\n");
-
-        // Generar los headers
-        for (String attribute : headers) {
-            sb.append("@CsvBindByPosition(position = ").append(Arrays.asList(headers).indexOf(attribute))
-                    .append(")\n");
-            sb.append("    private String ").append(attribute).append(";\n");
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
         }
-
-        // Generar los métodos getter y setter para cada attribute
-        for (String attribute : headers) {
-            String methodName = attribute.substring(0, 1).toUpperCase() + attribute.substring(1);
-            sb.append("\n");
-            sb.append("    public String get").append(methodName).append("() {\n");
-            sb.append("        return ").append(attribute).append(";\n");
-            sb.append("    }\n\n");
-            sb.append("    public void set").append(methodName).append("(String ").append(attribute).append(") {\n");
-            sb.append("        this.").append(attribute).append(" = ").append(attribute).append(";\n");
-            sb.append("    }\n");
-        }
-
-        // Override ToString
-        sb.append("\n");
-        sb.append("    @Override\n");
-        sb.append("    public String toString() {\n");
-        sb.append("        return \"").append(className).append(" [");
-        for (String attribute : headers) {
-            sb.append(attribute).append("=\" + ").append(attribute).append(" + \", ");
-        }
-        sb.append("]\";\n");
-        sb.append("    }\n");
-
-        // Cerrar la clase
-        sb.append("}");
-        
-        String outputDirectory = "C://Users//zullid//Documents//Java//convertrail//src//main//java//com//zullid//convertrail//csv//";
-        String filePath =  outputDirectory + "/" + className + ".java";
-        try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write(sb.toString());
-        }
-
-        System.out.println("Java file generated successfully: " + filePath);
-    }
-
-    public static void generateBean(File csvFile) throws IllegalStateException, IOException {
-        List<CsvBeans> beansAdicionales = new CsvToBeanBuilder<CsvBeans>(new FileReader(csvFile))
-                .withSkipLines(0)
-                .withType(CsvBeans.class)
-                .withSeparator(',')
-                .build()
-                .parse();
-
-        //beansAdicionales.forEach(System.out::println);
-        Gson gson = new Gson();
-        String path = "C://Users//zullid//Documents//pruebas_convertrail";
-        String json = gson.toJson(beansAdicionales);
-        try (FileWriter writer = new FileWriter(path + "//beansAdicionales.json")) {
-            writer.write(json);
-        }
-        System.out.println(json);
-
     }
 
 }
